910f02f7a4052d6797fd117f4cba78c5dbd71ea8
georgezefko <georgioszefkilis@gmail.com>
2023-05-14 10:09:21 +0200

    feat:added flake and pre-commit

--- stocks_crypto/data_loaders/lo_alpha_vantage.py
+++ stocks_crypto/data_loaders/lo_alpha_vantage.py
@@ -16,12 +16,28 @@ def load_data_from_api(*args, **kwargs):
     Template for loading data from API
     """
     KEY = get_secret_value('alphaVantageKey')
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=KEY'
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=full&apikey=KEY'
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
-    print(data)
-    return pd.DataFrame(data)
+    # Convert the time series data to a DataFrame
+    df = pd.DataFrame(data["Time Series (Daily)"]).T
+
+    # Convert index to datetime
+    df.index = pd.to_datetime(df.index)
+
+    # Rename columns for clarity
+    df.columns = ['open', 'high', 'low', 'close', 'volume','adjusted close','dividend amount','split coefficient']
+
+    # Convert the data type of each column to float or integer as appropriate
+    df = df.astype({'open': 'float', 'high': 'float', 'low': 'float', 'close': 'float', 'volume': 'int','adjusted close':'float','dividend amount':'float','split coefficient':'float'})
+
+    # Adding metadata to the DataFrame
+    for key, value in data["Meta Data"].items():
+        df[key] = value
+    
+    df.info()
+    return df
 
 
 @test
