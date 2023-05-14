5517f05006dad9f84fb92e414ac509d332c1401b
georgezefko <georgioszefkilis@gmail.com>
2023-05-14 11:02:48 +0200

    check

--- stocks_crypto/data_loaders/lo_alpha_vantage.py
+++ stocks_crypto/data_loaders/lo_alpha_vantage.py
@@ -1,13 +1,13 @@
 import io
+
 import pandas as pd
 import requests
-if 'data_loader' not in globals():
+
+if "data_loader" not in globals():
     from mage_ai.data_preparation.decorators import data_loader
-if 'test' not in globals():
+if "test" not in globals():
     from mage_ai.data_preparation.decorators import test
 from mage_ai.data_preparation.shared.secrets import get_secret_value
-
-
 
 
 @data_loader
@@ -15,8 +15,8 @@ def load_data_from_api(*args, **kwargs):
     """
     Template for loading data from API
     """
-    KEY = get_secret_value('alphaVantageKey')
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=full&apikey=KEY'
+    KEY = get_secret_value("alphaVantageKey")
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=full&apikey=KEY"
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
@@ -27,15 +27,35 @@ def load_data_from_api(*args, **kwargs):
     df.index = pd.to_datetime(df.index)
 
     # Rename columns for clarity
-    df.columns = ['open', 'high', 'low', 'close', 'volume','adjusted close','dividend amount','split coefficient']
+    df.columns = [
+        "open",
+        "high",
+        "low",
+        "close",
+        "volume",
+        "adjusted close",
+        "dividend amount",
+        "split coefficient",
+    ]
 
     # Convert the data type of each column to float or integer as appropriate
-    df = df.astype({'open': 'float', 'high': 'float', 'low': 'float', 'close': 'float', 'volume': 'int','adjusted close':'float','dividend amount':'float','split coefficient':'float'})
+    df = df.astype(
+        {
+            "open": "float",
+            "high": "float",
+            "low": "float",
+            "close": "float",
+            "volume": "int",
+            "adjusted close": "float",
+            "dividend amount": "float",
+            "split coefficient": "float",
+        }
+    )
 
     # Adding metadata to the DataFrame
     for key, value in data["Meta Data"].items():
         df[key] = value
-    
+    # return df information
     df.info()
     return df
 
