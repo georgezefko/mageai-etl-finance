034eb1cfd316f19d6465395d4cbdc9c3fc1d60ea
georgezefko <georgioszefkilis@gmail.com>
2023-06-20 22:37:29 +0200

    feat(pipelines):broke down pipeline

--- stocks_crypto/data_exporters/mergedexportaggr.py
+++ stocks_crypto/data_exporters/mergedexportaggr.py
@@ -20,6 +20,10 @@ def export_data(data, *args, **kwargs):
         Optionally return any object and it'll be logged and
         displayed when inspecting the block run.
     """
+    # specify table by using global variable
+    stored_table = kwargs["tableName"]
+    date = kwargs["date"]
+    print(stored_table)
 
     engine = create_engine(
         "duckdb:///stockapp.duckdb",
@@ -39,12 +43,11 @@ def export_data(data, *args, **kwargs):
     # Drop the table
     # stock_aggr.drop(engine)
     # If the table doesn't exist, create it
-    if not "stock_aggr" in inspector.get_table_names():
-
-        data.to_sql("stock_aggr", engine)
+    if not stored_table in inspector.get_table_names():
+        data.to_sql(stored_table, engine)
 
     # Reflect the table
-    stock_aggr = Table("stock_aggr", metadata, autoload_with=engine)
+    stored = Table(stored_table, metadata, autoload_with=engine)
 
     # Print database URL
     print(f"Database URL: {engine.url}")
@@ -53,32 +56,32 @@ def export_data(data, *args, **kwargs):
     print(f"Table Names: {inspector.get_table_names()}")
 
     # Print schema of the table 'daily_adjusted'
-    print(f"Schema of 'stock_aggr': {inspector.get_columns('stock_aggr')}")
+    print(f"Schema of '{stored_table}': {inspector.get_columns(stored_table)}")
 
     # Load the current data from the table
-    stmt = select([stock_aggr])
+    stmt = select([stored])
     current_data = pd.read_sql_query(stmt, con=engine)
 
     # Print first five rows
     print(f"First five rows: \n{current_data.head()}")
 
     # Filter out rows in data that already exist in current_data
-
     unique_rows = data[
-        ~data.set_index(["symbol", "Year"]).index.isin(
-            current_data.set_index(["symbol", "Year"]).index
+        ~data.set_index(["symbol", date]).index.isin(
+            current_data.set_index(["symbol", date]).index
         )
     ]
 
     print(f"Unique rows are:{len(unique_rows)}")
+
     # Assuming the 'index' column is not necessary, let's drop it
     current_data = current_data.drop(columns=["index"])
 
     # Insert unique_rows into the table
-    unique_rows.to_sql("stock_aggr", con=engine, if_exists="append", index=False)
+    unique_rows.to_sql(stored_table, con=engine, if_exists="append", index=False)
 
     # Group by symbol
-    stmt = select([stock_aggr.c.symbol, func.count()]).group_by(stock_aggr.c.symbol)
+    stmt = select([stored.c.symbol, func.count()]).group_by(stored.c.symbol)
     results = session.execute(stmt).fetchall()
 
     print(results)
---
+++ stocks_crypto/data_loaders/insertshares.py
@@ -0,0 +1,34 @@
+if "data_loader" not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if "test" not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data(*args, **kwargs):
+    """
+    Template code for loading data from any source.
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your data loading logic here
+
+    engine = create_engine(
+        "duckdb:///stockapp.duckdb",
+        connect_args={"read_only": False, "config": {"memory_limit": "1gb"}},
+    )
+
+    # Select all data from the table
+    query = f"SELECT * FROM adjustD"
+    data = pd.read_sql_query(query, con=engine)
+
+    return data
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, "The output is undefined"
