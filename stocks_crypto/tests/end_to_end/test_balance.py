aa403fd0c12da019f3a6ddda7fc9ab9242e687f7
georgezefko <georgioszefkilis@gmail.com>
2023-06-22 22:21:28 +0200

    docs:added dashboard gif

--- stocks_crypto/tests/end_to_end/test_balance.py
+++
@@ -1,9 +0,0 @@
-from mage_ai.data_preparation.models.pipeline import Pipeline
-from mage_ai.tests.base_test import TestCase
-
-
-class balanceSheetTest(TestCase):
-    def test_pipeline_execution(self):
-        pipeline = Pipeline.get("balancesheet")
-        print("kwargs", pipeline)
-        pipeline.execute_sync()
